0a79402de25246148a1556d4c7c76d4cd863d531
Jordan Tribley <jtribley@fanatics.com>
2016-07-07 17:31:12 -0400

    Changes at the end of part 3

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
+// Below is ES5 syntax, above is ES6 syntax
+//var render - require('react-dom').render
 
+render(
+	// define the encompassing component,
+	// DOM element we want to mount it to
+	<App/>,
+	document.getElementById('app')
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react'
+// Curly braces: Allows us to not need to call React.Component
 
+// Alternate typing of the below line...
+//var App = React.createClass({
+class App extends Component {
+
+	render() {
+		return <div>This is definitely a React app now!</div>
+	}
+
+}
+
+export default App
